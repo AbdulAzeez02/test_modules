0b875383d6f7517968e87ac7be6d20cb0a9a1cf0
AbdulAzeez02 <135802104+AbdulAzeez02@users.noreply.github.com>
2024-03-24 15:31:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0, <4.0.0"  # Use a version constraint
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
