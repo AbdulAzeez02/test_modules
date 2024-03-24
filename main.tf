e7e5c2647c40bc39b2026e7a07c374becc5f00fa
AbdulAzeez02 <135802104+AbdulAzeez02@users.noreply.github.com>
2024-03-24 15:33:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.0.0, <4.0.0"  # Use a version constraint
+      version = ">=2.0.0, <3.0.0"  # Use a version constraint
     }
   }
 }
