62946e7233b965dbc5e871a307a4d1df6a8624ca
AbdulAzeez02 <135802104+AbdulAzeez02@users.noreply.github.com>
2024-03-24 15:31:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"  # Use the specific version you want
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
-module "example" {
-  source = "github.com/AbdulAzeez02/test_modules?ref=main"
-
-  resource_group_name = "my-resource-group"
-  location            = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
